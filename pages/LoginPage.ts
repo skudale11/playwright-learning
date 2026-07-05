d93143f047d903db402015394957f5213149e81e
skudale10 <139463080+skudale10@users.noreply.github.com>
2026-07-05 23:00:25 +0200

    Day 8 - created LoginPage POM

---
+++ pages/LoginPage.ts
@@ -0,0 +1,20 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  async goto() {
+    await this.page.goto('https://practicesoftwaretesting.com');
+  }
+
+  async login(email: string, password: string) {
+    await this.page.getByRole('link', { name: 'Sign in' }).click();
+    await this.page.getByLabel('Email address').fill(email);
+    await this.page.getByPlaceholder('Your password').fill(password);
+    await this.page.getByRole('button', { name: 'Login' }).click();
+  }
+
+  async getErrorMessage() {
+    return this.page.getByText('Invalid email or password');
+  }
+}
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,25 +1,29 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
 
 test('homepage has correct title', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com');
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
   await expect(page).toHaveTitle(/Practice Software Testing/);
 });
 
 test('user can log in', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com');
-  await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByLabel('Email address').fill('customer@practicesoftwaretesting.com');
-  await page.getByPlaceholder('Your password').fill('welcome01');
-  await page.getByRole('button', { name: 'Login' }).click();
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login(
+    'customer@practicesoftwaretesting.com',
+    'welcome01'
+  );
   await expect(page).toHaveURL(/account/);
-  //await expect(page.getByText('Jane Doe')).toBeVisible();
 });
 
 test('user sees error with wrong password', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com');
-  await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByLabel('Email address').fill('customer@practicesoftwaretesting.com');
-  await page.getByPlaceholder('Your password').fill('wrongpassword');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page.getByText('Invalid email or password')).toBeVisible();
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login(
+    'customer@practicesoftwaretesting.com',
+    'wrongpassword'
+  );
+  const error = await loginPage.getErrorMessage();
+  await expect(error).toBeVisible();
 });
