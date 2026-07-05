69677961ba7207201c282cb6c4576c9d9ebe391e
skudale10 <139463080+skudale10@users.noreply.github.com>
2026-07-05 23:18:38 +0200

    CI - use robust prodcut locator to fix CI failure

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('add products to cart', async ({ page }) => {
+test('user can add a product to cart', async ({ page }) => {
   await page.goto('https://practicesoftwaretesting.com/');
   await page.locator('[data-test="nav-categories"]').click();
-  await page.locator('[data-test="product-01KWQEX7PGB7FJVDHJ54Z6ST7W"]').click();
+  await page.locator('[data-test^="product-"]').first().click();
   await page.locator('[data-test="add-to-cart"]').click();
+  await expect(page).toHaveURL(/product/);
 });
